4425cf3cc28793ee54b69371e01dfd906b527e94
amir arab <amir4rab@github.com>
2021-08-09 11:55:12 +0430

    added login page

---
+++ pages/login/index.js
@@ -0,0 +1,39 @@
+import { getSession, signIn } from 'next-auth/client';
+import classes from './loginPage.module.scss';
+
+function LoginPage() {
+    return (
+        <div className={ classes.loginPage }>
+            <h1 className={ classes.title }>
+                Login
+            </h1>
+            <div className={ classes.loginBox }>
+                <h3 className={ classes.subTitle }>
+                    login options
+                </h3>
+                <button
+                    className={ classes.btn }
+                    onClick={ _ => signIn('google') }
+                >
+                    sing in with google
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export const getServerSideProps = async (context) => {
+    const session = await getSession(context);
+
+    if( session !== null ) return {
+        redirect: {
+            destination: '/profile',
+        },
+    }
+
+    return {
+      props: {}, // will be passed to the page component as props
+    }
+};
+
+export default LoginPage;
