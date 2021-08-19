5105ad86f3646c9ed3416b2d6d4ab3cc0a8efcc1
amir arab <amir4rab@github.com>
2021-08-19 17:07:26 +0430

    social media box added

---
+++ components/userSettings/socialMediaBox/socialMediaBox.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import SocialMediaPopup from '../../socialMediaPopup/socialMediaPopup';
+import SocialItem from './socialItem/socialItem';
+import classes from './socialMediaBox.module.scss';
+
+function SocialMediaBox({ socialArr, setSocialArr }) {
+    const [ displayPopup, setDisplayPopup ] = useState(false);
+
+    const addToList = (obj) => {
+        setSocialArr(oldArr => ([
+            obj,
+            ...oldArr
+        ]))
+    }
+    const removeFromList = (instance) => {
+        setSocialArr(oldArr => ([
+            ...oldArr.filter(arrItem => arrItem.instance !== instance)
+        ]))
+    }
+
+    const addNewSocialEvent = () => {
+        setDisplayPopup(true);
+    }
+
+    return (
+        <div className={ classes.socialMediaBox }>
+            {
+                socialArr.map(socialMedia => <SocialItem data={ socialMedia } deleteItem={ removeFromList } key={ socialMedia.instance } />)
+            }
+            <div className={ classes.addNew }>
+                <button className={ classes.addButton } onClick={ addNewSocialEvent }>
+                    + Add new social
+                </button>
+            </div>
+            <SocialMediaPopup submitEvent={ addToList } displayPopup={ displayPopup } setDisplayPopup={ setDisplayPopup }  />
+        </div>
+    )
+}
+
+export default SocialMediaBox
