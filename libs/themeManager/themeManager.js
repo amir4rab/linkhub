1ab45ca4227b99aaa67ddf47575a90b6e4fdcb0e
amir arab <amir4rab@github.com>
2021-08-20 12:23:12 +0430

    validation added

--- libs/themeManager/themeManager.js
+++ libs/themeManager/themeManager.js
@@ -1,26 +1,30 @@
 export const themesArr = [
     {
         'name': 'mermaid',
-        'colorHexCode': '#CC99FF',
+        'hexCode': '#CC99FF',
         'class': 'theme0',
     },
     {
         'name': 'sky blue',
-        'colorHexCode': '#00D2D3',
+        'hexCode': '#00D2D3',
         'class': 'theme1',
     },
     {
         'name': 'mild pink',
-        'colorHexCode': '#FFB8B8',
+        'hexCode': '#FFB8B8',
         'class': 'theme2',
     }
 ];
 
 export const getClassByName = ( colorName, mainClass ) => {
     const colorObj = themesArr.find(colorObj => colorObj.name === colorName);
+    if ( colorObj === undefined ) return mainClass[themesArr[0].class];
     return mainClass[colorObj.class];
 }
 
 export const getClassByClass = ( colorClass, mainClass ) => mainClass[colorClass];
 
-export const getClassByIndex = ( index, mainClass ) => mainClass[themesArr[index].class];
+export const getClassByIndex = ( index, mainClass ) => {
+    if( index + 1 >= themesArr.length ) return mainClass[themesArr[0].class];
+    return mainClass[themesArr[index].class]
+};
