0721f2028fed4c5be0379d009fa14d1325141e73
amir arab <amir4rab@github.com>
2021-08-23 09:29:39 +0430

    removed unused codes

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,14 @@
 import '../styles/globals.scss';
 import Layout from '../layout/layout';
 import { Provider } from 'next-auth/client';
-import { UserDataProvider } from './../context/userDataContext/userDataContext';
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
-      <UserDataProvider>
         <Layout>
           <Component {...pageProps} />
         </Layout>
-      </UserDataProvider>  
     </Provider>
   );
 }
