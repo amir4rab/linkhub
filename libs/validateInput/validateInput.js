09f4a30ef7392772b2a03a460d47597f7a14d042
amir arab <amir4rab@gmail.com>
2021-09-05 10:15:20 +0430

    change in validation

--- libs/validateInput/validateInput.js
+++ libs/validateInput/validateInput.js
@@ -1,35 +1,40 @@
-export const validateInput = (input, maxLength = 24 ) => {
-    const regex = /^[0-9a-zA-Z ]+$/g;
-    if ( regex.exec(input) && input.length < maxLength ) {
-        return true;
+const labelRegex = /^[A-Za-z0-9_-\s]+$/;
+const idRegex = /^[A-Za-z0-9_]+$/;
+const numRegex = /^[0-9]+$/;
+const textRegex = /^[A-Za-z0-9_-\s]+$/;
+
+export const validateInput = ( input, maxLength = 24, isId = false ) => {
+    if ( isId ){
+        if ( idRegex.exec(input) && input.length < maxLength ) {
+            return true;
+        } else {
+            return false;
+        };
     } else {
-        return false;
-    };
+        if ( labelRegex.exec(input) ) {
+            return true;
+        } else {
+            return false;
+        };
+    }
 }
 
-export const looseValidateInput = (input, maxLength = 150 ) => {
-    const regex = /^[0-9a-zA-Z .  , : ;]+$/g;
-    if ( regex.exec(input) && input.length < maxLength ) {
-        return true;
-    } else {
-        return false;
-    };
-}
+export const looseValidateInput = (input, maxLength = 150 ) =>  ( textRegex.exec(input) && input.length < maxLength );
 
-export const isNum = (input) => {
-    const regex = /^[0-9]+$/g;
-    if ( regex.exec(input) ) {
-        return true;
-    } else {
-        return false;
-    };
-}
+export const isNum = (input) => numRegex.test(input);
 
-export const validateSocialArrItem = (input) => {
-    const regex = /^[0-9a-zA-Z -]+$/g;
-    if ( regex.exec(input) ) {
-        return true;
+export const validateSocialArrItem = ( input, isId = false ) => {
+    if( isId ) {
+        if ( idRegex.exec(input) ) {
+            return true;
+        } else {
+            return false;
+        };
     } else {
-        return false;
-    };
+        if ( labelRegex.exec(input) ) {
+            return true;
+        } else {
+            return false;
+        };
+    }
 }
